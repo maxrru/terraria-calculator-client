4b4a05fa6fef4b3e1b5cf6187a9a74cd5e7f12be
Max Ungar <maxrru.developer@gmail.com>
2018-02-22 00:27:24 +0100

    Minimal working template

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import { Alert } from 'react-bootstrap'
+
+export default class App extends Component {
+  render () {
+    return <Alert bsStyle="warning">This is my new react app</Alert>
+  }
+}
